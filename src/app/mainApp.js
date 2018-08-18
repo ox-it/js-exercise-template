773ffbc3eae6f2cf8203756fd4d6a495e0069ece
markos ntoumpanakis <markos.ntoumpanakis@it.ox.ac.uk>
2018-08-18 14:32:41 +0100

    write a simple angular controller

--- src/app/mainApp.js
+++ src/app/mainApp.js
@@ -1,12 +1,24 @@
-// import React from 'react';
+import angular from 'angular';
 require('../public/stylesheets/style0.css');
 
 var app = document.getElementById('app2');
 if(!app) {
-    //create if needed
     var body = document.getElementsByTagName('body')[0];
+    body.setAttribute('ng-app', 'myApp');
     var appElement = document.createElement('div');
-    appElement.id = 'app2'
-    appElement.innerHTML = 'wellcome !!!';
+    appElement.id = 'app2';
+    appElement.setAttribute("ng-controller", "MyCtrl");
+    appElement.innerHTML = 'Hello, {{name}}!';
     body.appendChild(appElement);
+}
+
+// <body ng-app='myApp' binds to the app being created below.
+var app = angular.module('myApp', []);
+
+// Register MyController object to this app
+app.controller('MyCtrl', ['$scope', MyController]);    
+
+// We define a simple controller constructor.
+function MyController($scope) {
+    $scope.name = 'World!!!!';
 }
